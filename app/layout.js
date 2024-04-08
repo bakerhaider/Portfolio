b78b577ff5c935ef37a11accaee17c34ab875619
BakerAlblam <gallardo0@outlook.com>
2024-04-08 19:48:24 +0200

    analytics

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Footer from './components/footer';
 import Navbar from './components/navbar';
 import './css/card.scss';
 import './css/globals.scss';
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
+          <Analytics />
         </main>
        
       </body>
